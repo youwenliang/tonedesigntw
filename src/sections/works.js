18819a7b9c68f612cbb6b4a59a12459c0210fb82
Mark Liang <mliang@mozilla.com>
2019-05-21 18:25:50 +0800

    update works img ratio

--- src/sections/works.js
+++ src/sections/works.js
@@ -7,11 +7,18 @@ class Works extends Component {
     var sectionStyle = {
   		background: "#ffffff"
   	}
+  	var contentStyle = {
+  		minWidth: "1540px"
+  	}
     return (
       <section className="works" style={sectionStyle}>
       	<div className="content ph4 mw9 center tc">
       	  <Header title="Our Portfolio" color="#4c5b7f"/>
-      	  <img className="mb4" src="https://fakeimg.pl/1920x720/?text=Portfolio Video"/>
+      	  <div className="flex justify-center">
+      	  	<div style={contentStyle}>
+      	  		<img className="mb4" width="100%" src="https://fakeimg.pl/1920x720/?text=Portfolio Video"/>
+      	  	</div>
+      	  </div>
       	  <Button content="Contact!"/>
       	  <h2 className="mt4 f3 i fw3 normal color-content">Clinging to a one-point</h2>
       	</div>
